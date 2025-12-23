b68c3340e25948da12b65718ec0dc880a1d353b1
Bishal roy <144330743+roybishal362@users.noreply.github.com>
2025-12-23 12:11:42 +0530

    Update import path for PromptTemplate

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.documents import Document
 
